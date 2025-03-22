b61e0e4a4f19ec7b2cc8dce215cbc821164eb39e
Soumik Dey <soumik.dey@vehere.com>
2025-03-22 16:00:04 +0000

    my-branch->simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -3,10 +3,13 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "Hello world in C++" << endl;
+    cout << "First program in C++" << endl;
     cout << "Enter two intergers: ";
     cin >> a >> b;
     int c = a + b;
     cout << "Sum = " << c << endl;
+    c = a * b;
+    cout << "Product = " << c << endl;
+
     return 0;
 }
